1bf126931a1ea79078ba810c2d1999f03115e2d8
MJAOUA <MJAOUA@DESKTOP-TE8KGHO>
2021-08-16 00:43:06 +0100

    1508 update

--- src/main/java/com/banking/controllers/UserController.java
+++ src/main/java/com/banking/controllers/UserController.java
@@ -1,7 +1,5 @@
 package com.banking.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
